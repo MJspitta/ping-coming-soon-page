dd83f84ff1f570a9c02aa39af1da047e97b80dd7
Madu Jang <madu.jang93@gmail.com>
2025-06-06 01:26:53 +0100

    responsive design

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const emailInput = document.querySelector('input');
-const btn = document.querySelector('button');
-const errorMessage = document.getElementById('error');
+const emailInput = document.querySelector("input");
+const btn = document.querySelector("button");
+const errorMessage = document.getElementById("error");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   if (emailInput.checkValidity()) {
     errorMessage.textContent = "";
-    emailInput.classList.remove('error');
+    emailInput.classList.remove("error");
   } else {
     errorMessage.textContent = "Please provide a valid email address";
+
     errorMessage.style.color = "red";
-    emailInput.classList.add('error');
+    emailInput.classList.add("error");
   }
-})
+});
